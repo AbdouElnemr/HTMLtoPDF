47369b439d1d34097293b87e8bf9a26f40b0af7a
elnemr <elnemr@elnemr.com>
2018-04-08 00:40:15 +0200

    Setting Arabic Language

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
 <?php
 
-require_once('/var/www/html/pdfProject/TCPDF/tcpdf.php');
+require_once('/var/www/html/pdfProject/tcpdf/tcpdf.php');
 
-$pdf = new \TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT,  "letter", true, 'UTF-8', false);
 
 $html = file_get_contents('/var/www/html/pdfProject/index.html');
 
+$pdf->AddPage();
+  
+$pdf->SetFont('amirii',  16);
 
-  $pdf->AddPage();
+$pdf->WriteHTML($html, true, false, true, false, '');
 
-  $pdf->SetFont('cid0cs',  15);
-
-  $pdf->WriteHTML($html, true, false, true, false, '');
-
-
-$pdf->Output('/var/www/html/pdfProject/example.pdf', 'I');
+$pdf->Output('example.pdf', 'I');
